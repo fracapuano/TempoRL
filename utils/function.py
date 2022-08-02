f29a72e5b0847616f45e666c9eed92e29f55cc48
Francesco Capuano <francesco_capuano@aol.com>
2022-08-02 10:28:15 +0200

    initial commit

--- utils/function.py
+++ utils/function.py
@@ -5,3 +5,9 @@ def rosen(x: np.array):
     This function returns the (scalar) value of the generalized n-dimensional rosenbrock function at any point x of size (n,)
     """
     return sum(100.0*(x[1:] - x[:-1]**2.0)**2.0 + (1 - x[:-1])**2.0)
+
+def gradient(f:function, x:np.array, mode:str="forward")->np.array: 
+    """
+    This function numerically computes the gradient of the given function. 
+    
+    """
