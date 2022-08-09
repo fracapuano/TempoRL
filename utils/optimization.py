f8a026e65c25fdf70577266403ed7e92a972b0ce
Francesco Capuano <francesco_capuano@aol.com>
2022-08-09 09:39:46 +0200

    1) Now find_alpha function only implements armijo backtracking, line search has been removed to ensure full compatibility. 2) Default parameters have been changed so to be more reasonable 3) Objective function value has been introduced in the loops 4) Verbose message has been introduced

--- utils/optimization.py
+++ utils/optimization.py
@@ -1,9 +1,9 @@
 import numpy as np
 from numpy.linalg import norm
-from utils.numerical_diff import *
-from scipy.optimize import line_search
+from numerical_diff import *
+from scipy.optimize import minimize_scalar
 import matplotlib.pyplot as plt
-from scipy.sparse.linalg import gmres
+from scipy.sparse.linalg import gmres, cg
 from typing import Tuple
 
 def find_alpha(
@@ -11,11 +11,10 @@ def find_alpha(
     grad_f, 
     xk:np.array,
     pk:np.array,
-    alpha_0:float=0.5,  
+    alpha_0:float=1,  
     c1:float=1e-4, 
     rho:float=8e-1, 
-    maxit_bt:int=10, 
-    maxit_wolfe=50)->float: 
+    maxit_bt:int=10)->float: 
     """This function finds the optimal steplenght using a combination of Wolfe conditions and (in case these conditions could not be matched) and
     regular Armijo conditions using backtrack.
 
@@ -28,21 +27,18 @@ def find_alpha(
         c1 (float, optional): Slope of the Armijo line. Defaults to 1e-4.
         rho (float, optional): Backtracking factor used to scale down the steplength. Defaults to 8e-1.
         maxit_bt (int, optional): Maximal number of backtrack iterations. Defaults to 10.
-        maxit_wolfe (int, optional): Maximal number of iterations to verify strong Wolfe conditions. Defaults to 50.
 
     Returns:
         float: optimal steplength according to line search
-    """
-    
-    alpha_k = line_search(f, grad_f, xk, pk, maxiter = maxit_wolfe)[0]
-    if alpha_k is None: # find alpha using regular Armijo backtrack
-        alpha_k = alpha_0
-        armijo_line = f(xk) - c1*alpha_k*(grad_f(xk) @ pk)
-        bt_it = 0
+    """    
+    # find alpha using regular Armijo backtrack
+    armijo_line = lambda alpha_k: f(xk - alpha_k * pk) - f(xk) - c1*alpha_k*(grad_f(xk) @ pk)
+    alpha_k = alpha_0
+    bt_it = 0
 
-        while f(xk - alpha_k*pk) > armijo_line and bt_it < maxit_bt: 
-            alpha_k = rho*alpha_k
-            bt_it = bt_it + 1
+    while armijo_line(alpha_k) > 0 and bt_it < maxit_bt: 
+        alpha_k = rho*alpha_k
+        bt_it = bt_it + 1
     
     return alpha_k
 
@@ -56,9 +52,9 @@ def newton(
     alpha_0:float=1, 
     c1:float=1e-4, 
     rho:float=8e-1, 
-    maxit_bt:int=50,
-    maxit_wolfe:int=50,
-    gmres_maxit:int=int(1e3))->Tuple[np.array, np.array]:
+    maxit_bt:int=10,
+    gmres_maxit:int=int(1e2), 
+    verbose:int=0)->Tuple[np.array, np.array]:
     """This function implements the Newton Method for non linear optimization using a mixture of strong Wolfe conditions 
     and Armijo backtracking to carry out linesearch. 
 
@@ -72,16 +68,16 @@ def newton(
         alpha_0 (float, optional):  Initial value for the steplength prior to backtracking. Defaults to 1.
         c1 (float, optional): Slope of the Armijo line. Defaults to 1e-4.
         rho (float, optional): Backtracking factor used to scale down the steplength. Defaults to 8e-1.
+        maxit_bt (int, optional): Maximal number of backtrack iterations to satisfy Armijo condition. Defaults to int(50).
         maxit (int, optional): Maximal number of iterations in the iterative method considered. Defaults to int(1e4).
-        maxit_wolfe (int, optional): Maximal number of iterations to satisfy strong Wolfe conditions. Defaults to 10.    
-        gmres_maxit (int, optional): Maximal number of iterations to solve the inner Newton Method. Defaults to int(1e3).
-
+        verbose (int, optional): Verbose parameter. Printing optimization route information if verbose is greater than 0. 
     Returns:
         Tuple[np.array, np.array]: 
             x_star: the optimal point to which the optimization route has converged
             grads: the norm of the gradient over the optimization route as well
     """
     xk = x0
+    fxk = f(xk)
     grad_xk = grad_f(xk)
     grad_norm = norm(grad_xk)
     grads = np.zeros(maxit)
@@ -89,18 +85,22 @@ def newton(
     k = 0
     while k < maxit and grad_norm > tolgrad: 
         it_time = time.time()
+        xold = xk
+        alpha_k = alpha_0
         hess_xk = hess_f(xk)
         grad_xk = grad_f(xk)
+        fxk = f(xk)
         grad_norm = norm(grad_xk)
         grads[k] = grad_norm
 
-        pk, _ = gmres(hess_xk, -grad_xk, maxiter=gmres_maxit)
-        alpha_k = find_alpha(f, grad_f, xk, pk, c1=c1, alpha_0=alpha_0, rho=rho, maxit_bt=maxit_bt, maxit_wolfe=maxit_wolfe)
+        pk, _ = gmres(hess_xk, -grad_xk, maxit=gmres_maxit)     
+        alpha_k = find_alpha(f, grad_f, xk, pk, c1=c1, alpha_0=alpha_0, rho=rho, maxit_bt=maxit_bt)
 
         xk = xk + alpha_k * pk
         k += 1
-        print("It {} - gradnorm {:.4e} - obtained in {} s".format(k, grad_norm, time.time()-it_time))
-    
+        if verbose > 0:
+            print("It {} - gradnorm {:.4e} - function value {:.4e} - solution difference {:.4e} - obtained in {} s".format(k, grad_norm, fxk, norm(xk - xold), time.time()-it_time))
+
     # "cutting" grads to the correct shape
     grads = grads[:k]
     print(f"Number of its: {k}")
@@ -112,11 +112,11 @@ def pr_plus(
     x0:np.array, 
     tolgrad:float=1e-6, 
     maxit:int=int(1e4), 
-    alpha_0:float=0.5,
+    alpha_0:float=1,
     c1:float=1e-4, 
     rho:float=0.8, 
-    maxit_bt:int=50,
-    maxit_wolfe:int=50)->Tuple[np.array, np.array]:
+    maxit_bt:int=10, 
+    verbose:int=0)->Tuple[np.array, np.array]:
     """This function implements the NonLinear Conjugate Gradient Method, in the Polak-Ribieré "+" variant proposed by Nocedal-Wright in
     "Numerical Optimization".
 
@@ -130,7 +130,7 @@ def pr_plus(
         c1 (float, optional): Slope of the Armijo line. Defaults to 1e-4.
         rho (float, optional): Backtracking factor used to scale down the steplength. Defaults to 0.8.
         maxit (int, optional): Maximal number of iterations in the iterative method considered. Defaults to int(1e4).
-        maxit_wolfe (int, optional): Maximal number of iterations to satisfy strong Wolfe conditions. Defaults to 10.
+        verbose (int, optional): Verbose parameter. Printing optimization route information if verbose is greater than 0. 
 
     Returns:
         Tuple[np.array, np.array]: 
@@ -148,7 +148,9 @@ def pr_plus(
     grad_norm = norm(grad_xk)
     pk = p0
     
-    while k < maxit and grad_norm > tolgrad: 
+    while k < maxit and grad_norm > tolgrad:
+        it_time = time.time()
+        xold = xk 
         grad_xk = grad_f(xk)
         grads[k] = norm(grad_xk)
 
@@ -157,17 +159,18 @@ def pr_plus(
         betaPR_k = max(0, (SDNew @ (SDNew - SDOld))/(SDOld @ SDOld))
         pk = SDNew + betaPR_k * pk
         # steplength selection
-        alpha_k = find_alpha(f, grad_f, xk, pk, c1=c1, alpha_0=alpha_0, rho=rho, maxit_bt=maxit_bt, maxit_wolfe=maxit_wolfe)
+        alpha_k = find_alpha(f, grad_f, xk, pk, c1=c1, alpha_0=alpha_0, rho=rho, maxit_bt=maxit_bt)
         
-        xk = xk + alpha_k * pk            
+        xk = xk + alpha_k * pk           
 
         SDOld = SDNew
         grad_norm = norm(SDNew)
         
         k += 1
+        if verbose > 0: 
+            print("It {} - gradnorm {:.4e} - function value {:.4e} - solution difference {:.4e} - obtained in {} s".format(k, grad_norm, f(xk), norm(xk - xold), time.time()-it_time))
     # "cutting" grads to the correct shape
     grads = grads[:k]
-    print(f"Number of its: {k}")
     return xk, grads
 
 def rosen_grad_handle(xk:np.array): 
@@ -203,13 +206,13 @@ def plot_gradient_norm(dim:int=10):
 
     print("Starting newton method...")
     start_time = time.time()
-    x_newton, grads_n = newton(rosen, rosen_grad_handle, rosen_hess_handle, x_start, maxit_bt=10, rho=3e-1, tolgrad=tolgrad)
+    x_newton, grads_n = newton(rosen, rosen_grad_handle, rosen_hess_handle, x_start, tolgrad=tolgrad, verbose = 1)
     completion = time.time() - start_time
     print("Newton complete! (elapsed in {:.3f} s)".format(completion))
 
     print("Starting PR+ method...")
     start_time = time.time()
-    x_pr, grads_pr = pr_plus(rosen, rosen_grad_handle, x_start, maxit_bt=10, rho=3e-1, alpha_0 = 1, tolgrad=tolgrad)
+    x_pr, grads_pr = pr_plus(rosen, rosen_grad_handle, x_start, maxit_bt=10, rho=3e-1, alpha_0 = 1, tolgrad=tolgrad, verbose = 1)
     completion = time.time() - start_time
     print("PR+ complete! (elapsed in {:.3f} s)".format(completion))
 
