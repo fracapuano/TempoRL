73dd439910e5a1380faccb646c1104e360e4d26c
fracapuano <francesco_capuano@aol.com>
2022-08-29 15:18:41 +0200

    now physics implements a function to auto compute FWHM value

--- utils/physics.py
+++ utils/physics.py
@@ -169,106 +169,16 @@ def temporal_profile(frequency:np.array, field:np.array, phase:np.array, npoints
     else: 
         return time, intensity_time
 
-# L3 necessary information - retrieved from previous version - TO BE INTEGRATED SOON
-def theorical_phase(frequency:np.array, central_carrier:float)->np.array:
-    """This function returns the theorical phase coming out of a DAZZLER-SPIDER system considering specific input frequency and central carrier. The phase
-    is computed according to an expert-provided custom formula. The uoms of GDD, TOD and FOD are not SI units but are fs^2, fs^3 and fs^3. 
-
-    Args:
-        frequency (np.array): Array of frequencies considered. 
-        central_carrier (float): Central carrier frequency. Used to emit the phase.
-
-    Returns:
-        np.array: Theorical phase obtained considering the formula presented.
+def FWHM(x:np.array, y:np.array)->float: 
+    """This function computes the FWHM roots of a given signal.
+    Args: 
+        x (np.array): x-axis representation of the signal
+        y (np.array): y-axis representation of the signal
+    
+    Returns: 
+        float: value, in seconds, of FWHM.
     """
-    GDD, TOD, FOD = 30, 40, 50
-    alpha_GDD, alpha_TOD, alpha_FOD = 25e3, 30e3, 50e3
-
-    theorical_phase = \
-            (1/2)*(GDD*1000 - alpha_GDD)*1e-30*(2*np.pi*frequency - central_carrier)**2 + \
-            (1/6)*(TOD*1000-alpha_TOD)*1e-45*(2*np.pi*frequency - central_carrier)**3 + \
-            (1/24)*(FOD*1000 - alpha_FOD)*1e-60*(2*np.pi*frequency - central_carrier)**4
+    half_signal = y - (y.max() / 2)
+    half_spline = UnivariateSpline(x = x, y = half_signal, s = 0)
     
-    return theorical_phase
-
-def phase_expansions(frequency:np.array, phase:np.array, degree:int=4)->np.array:
-    """This function polynomially expand a phase with respect to input frequency and returns the polynomial coefficients in ascendind order (ordered by degree)
-
-    Args:
-        frequency (np.array): Array of frequencies considered. 
-        phase (np.array): Array containing the phase to be used in the polynomial fitting process.
-        degree (int, optional): Degree of the polynomial used. 
-
-    Returns:
-        np.array: The polynomial coefficients used to fit the polynomial. 
-    """
-    control_params = np.polyfit(frequency, phase, deg = degree)
-    return control_params[::-1]
-
-class PulseEmitter: 
-    def __init__(self, frequency:np.array, field:np.array, useEquation:bool=False, central_carrier:float=None)->None: 
-        self.frequency = frequency 
-        self.field = field
-        self.useEquation = useEquation
-        
-        if self.useEquation: 
-            if central_carrier is None: 
-                raise ValueError("Central carrier needed if the phase is being reconstructed using an equation")
-            else: 
-                self.central_carrier = central_carrier
-    
-    def phase_reconstruction(self, control_params:np.array)->np.array: 
-        return (self.frequency.reshape(-1,1) ** np.arange(start = 0, stop = len(control_params))) @ control_params
-
-    def phase_control(self, control_params:np.array)->np.array: 
-        if len(control_params)==3:
-            GDD, TOD, FOD = control_params # the control happens with parameters in fs^2, fs^4 and fs^4.
-            alpha_GDD, alpha_TOD, alpha_FOD = 25e3, 30e3, 50e3
-        else: 
-            raise ValueError("Please use GDD, TOD and FOD only if constructing the phase starting from equation.")
-        
-        theorical_phase = \
-            (1/2)*(GDD*1000 - alpha_GDD)*1e-30*(2*np.pi*self.frequency - self.central_carrier)**2 + \
-            (1/6)*(TOD*1000-alpha_TOD)*1e-45*(2*np.pi*self.frequency - self.central_carrier)**3 + \
-            (1/24)*(FOD*1000 - alpha_FOD)*1e-60*(2*np.pi*self.frequency - self.central_carrier)**4
-    
-        return theorical_phase
-
-    def temporal_profile(self, control_params:np.array, num_points:int=int(2e4))->np.array: 
-        """This function returns the temporal profile of a signal described in frequency and electric field given the considered control parameters. 
-        It applies padding to the arrays to increase the precision of the fft algorithm (since it increases the sample complexity).
-
-        Args:
-            control_params (np.array): The coefficients to reconstruct the phase according to a polynomial model.
-            num_points (int, optional): Number of points to be used in padding to increase sample complexity. Defaults to int(2e4).
-
-        Returns:
-            np.array: The time representation of the pulse. 
-        """
-        self.npoints_increment = num_points
-
-        spectrum_field = np.pad(self.field, (num_points//2, num_points//2), "constant", constant_values = (0,0))
-        if self.useEquation: 
-            spec_phase = self.phase_control(control_params)
-        else: 
-            spec_phase = self.phase_reconstruction(control_params)
-            
-        spectrum_phase = np.pad(spec_phase, (num_points//2, num_points//2), "constant", constant_values = (0,0))
-        
-        time_field = fftshift(fft(spectrum_field*np.exp(1j*spectrum_phase)))
-
-        pulse = time_field * np.conjugate(time_field)
-
-        return np.real(pulse / pulse.max())
-
-    def time_scale(self)->np.array:
-        """This function returns the time scale for the considered signal.
-
-        Returns:
-            np.array: Time scale of the signal (in femtoseconds)
-        """
-        step = np.diff(self.frequency)[0]
-        sample_points = len(self.field) + self.npoints_increment
-        time = fftshift(fftfreq(sample_points, d=abs(step)))
-
-        return time * 1e15
+    return (np.abs(half_spline.roots())).sum()
