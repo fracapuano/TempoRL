94ba62e9483fb8087d2584c3dfa6fc7bca2ea6fb
fracapuano <francesco_capuano@aol.com>
2023-03-19 14:21:03 +0100

    add: random policy env

--- opt.py
+++ opt.py
@@ -1,3 +1,8 @@
+"""
+Test a random policy on the laser environment.
+Built to test out different parts of the environment.
+"""
+from tqdm import tqdm
 from env.LaserEnv_v1 import LaserEnv_v1
 from env.env_utils import EnvParametrization
 
@@ -9,4 +14,23 @@ env = LaserEnv_v1(
     compressor_params = compressor_params, 
     B_integral = B_integral)
 
-print(env._get_info())
+print('State space:', env._observation_space)
+print('Action space:', env.action_space)
+
+n_episodes = 5
+render = True  # TODO: Implement custom renderer.  
+
+for episode in range(n_episodes):
+    done = False
+    observation = env.reset()	# Reset environment to initial state
+    episode_length = 0
+    while not done:  # Until the episode is over
+        action = env.action_space.sample()	# Sample random action
+        observation, reward, done, info = env.step(action)	# Step the simulator to the next timestep
+        episode_length += 1
+
+        if render:
+            env.render()
+        
+    print(f"Episode Length: {episode_length}")
+    print(info["L1Loss"])
