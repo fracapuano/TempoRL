7eb8a80a708ef1bda62dc507b603212221388108
fracapuano <francesco_capuano@aol.com>
2023-03-22 00:10:53 +0100

    add: RL algorithms dict

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,15 @@ from .physics import *
 from .preprocessing import *
 from .render import * 
 from .torch_utils import *
+
+from stable_baselines3 import PPO, SAC, A2C
+from sb3_contrib import TRPO
+
+
+# dictionary to test out diffrent algorithms
+reverseAlgoDict = {
+    "PPO": PPO, 
+    "SAC": SAC,
+    "A2C": A2C, 
+    "TRPO": TRPO
+}
