a2d2d33e3d9bb7b243674d4caa4308c447a511fe
fracapuano <francesco_capuano@aol.com>
2023-03-25 23:56:14 +0100

    remove: useless files

--- prova.py
+++
@@ -1,9 +0,0 @@
-from policy.policy import Policy
-from policy.callbacks import PulseTrainingCallback
-from env import build_default_env
-
-envs = build_default_env(n_envs=3, subprocess=True)
-policy = Policy(algo="ppo", env=envs)
-policy.train(
-    timesteps=5, callback_list=[PulseTrainingCallback(env=envs, n_eval_episodes=1)]
-)
