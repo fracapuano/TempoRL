fix: reduce number of units in net

policy/policy.py
@@ -44,7 +44,7 @@ class Policy:
     def create_model(self, algo, lr):
         if algo == 'ppo':
             policy_kwargs = dict(activation_fn=torch.nn.Tanh,
-                                 net_arch=dict(pi=[128, 128], vf=[128, 128]))
+                                 net_arch=dict(pi=[64, 64], vf=[64, 64]))
             model = PPO("MlpPolicy", 
                         self.env, 
                         policy_kwargs=policy_kwargs,
@@ -56,7 +56,7 @@ class Policy:
 
         elif algo == 'sac':
             policy_kwargs = dict(activation_fn=torch.nn.Tanh,
-                                 net_arch=dict(pi=[128, 128], qf=[128, 128]))
+                                 net_arch=dict(pi=[64, 64], qf=[64, 64]))
             model = SAC("MlpPolicy", 
                         self.env, 
                         policy_kwargs=policy_kwargs,
@@ -68,7 +68,7 @@ class Policy:
         
         elif algo == 'trpo': 
             policy_kwargs = dict(activation_fn=torch.nn.Tanh,
-                                 net_arch=dict(pi=[128, 128], qf=[128, 128]))
+                                 net_arch=dict(pi=[64, 64], qf=[64, 64]))
             model = TRPO("MlpPolicy", 
                         self.env, 
                         policy_kwargs=policy_kwargs,