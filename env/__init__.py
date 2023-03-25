27eebf73566e4c9b365385b00b00ab57d55af6ff
fracapuano <francesco_capuano@aol.com>
2023-03-25 19:43:11 +0100

    add: build default (vec)env function.

--- env/__init__.py
+++ env/__init__.py
@@ -2,3 +2,41 @@ from .BaseLaser import *
 from .env_utils import *
 from .LaserEnv_v1 import *
 from .LaserModel import *
+
+from stable_baselines3.common.vec_env.base_vec_env import VecEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
+from .LaserEnv_v1 import LaserEnv_v1
+from stable_baselines3.common.monitor import Monitor
+
+def build_default_env(n_envs:int=1, subprocess:bool=True)->VecEnv:
+    """Simply builds an env using default configuration for the environment.
+
+    Args: 
+        n_envs (int, optional): Number of different envs to instantiate (using VecEnvs). Defaults to 1.
+        subprocess (bool, optional): Whether or not to create multiple copies of the same environment using 
+                                     suprocesses (so that multiple envs can run in parallel). When False, uses
+                                     the usual DummyVecEnv. Defaults to True.
+    
+    Returns: 
+    """
+    # define xi
+    params = EnvParametrization()
+    compressor_params, bounds, B_integral = params.get_parametrization()
+    # define environment (on top of xi)
+    def make_env():
+        env = LaserEnv_v1(
+            bounds = bounds, 
+            compressor_params = compressor_params, 
+            B_integral = B_integral)
+        # remove!
+        env.MAX_STEPS = 2
+        env = Monitor(env)
+        return env
+
+    # vectorized environment, wrapped with Monitor
+    if subprocess:
+        env = SubprocVecEnv([make_env for _ in range(n_envs)], start_method="fork")
+    else: 
+        env = DummyVecEnv([make_env for _ in range(n_envs)])
+
+    return env
