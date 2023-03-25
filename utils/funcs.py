cf6b53f06400b3d8087a6b876953f824412cf876
fracapuano <francesco_capuano@aol.com>
2023-03-25 23:51:13 +0100

    add: useful function

--- utils/funcs.py
+++ utils/funcs.py
@@ -3,6 +3,18 @@ This script implements basic functions useful for the software engineering part 
 """
 from pathlib import Path
 import json
+import math
+
+def to_scientific_notation(number:float)->str:
+    """
+    Converts number to scientific notation with one digit.
+    For instance, 5000 becomes '5e3' and 123.45 becomes '1.2e2'
+    """
+    exponent = int(round(math.log10(number)))
+    mantissa = round(number / (10 ** exponent), 1)
+    
+    # Format as string
+    return f"{mantissa}e{exponent}"
 
 def get_project_root() -> Path:
     """
