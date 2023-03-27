ba9de07cfb4bdc1fd00b4ecbe51f3d02aca2d7fa
fracapuano <francesco_capuano@aol.com>
2023-03-28 00:30:51 +0200

    add: render mode to args and build non vectorized env for rendering

--- env/__init__.py
+++ env/__init__.py
@@ -6,10 +6,12 @@ from .env_utils import *
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv
 from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
 from .LaserEnv_v1 import LaserEnv_v1
+from .LaserEnv_v2 import LaserEnv_v2
 from stable_baselines3.common.monitor import Monitor
 
 VersionDict = dict(
     v1=LaserEnv_v1,
+    v2=LaserEnv_v2
 )
 
 def build_default_env(
@@ -17,6 +19,7 @@ def build_default_env(
         n_envs:int=1, 
         subprocess:bool=True, 
         device="cpu",
+        render_mode="rgb_array",
         **kwargs)->VecEnv:
     """Simply builds an env using default configuration for the environment.
 
@@ -38,6 +41,7 @@ def build_default_env(
             bounds=bounds,
             compressor_params=compressor_params, 
             B_integral=B_integral, 
+            render_mode=render_mode,
             device=device,
             # kwargs differentiate between different versions
             env_kwargs=kwargs)
@@ -51,4 +55,36 @@ def build_default_env(
     else: 
         env = DummyVecEnv([make_env for _ in range(n_envs)])
 
+    return env
+
+def get_default_env(
+        version:str="v1",
+        device="cpu",
+        render_mode="rgb_array",
+        **kwargs)->Abstract_BaseLaser:
+    """Simply builds an env using default configuration for the environment.
+
+    Args: 
+        version (str, optional): Environment version. Used to distinguish between version1 and version2. Defaults to "v1".
+        device (str, optional): Device on which to run the environment. Defaults to "cpu".
+        render_mode (str, optional): Render mode. Defaults to "rgb_array".
+    
+    Returns: 
+        Abstract_BaseLaser: gym.Env of the laser considered.
+    """    
+    # define xi
+    params = EnvParametrization()
+    compressor_params, bounds, B_integral = params.get_parametrization()
+    # define environment (on top of xi)
+    env = VersionDict[version](
+        # first four arguments are always the same for all LaserEnv(s)
+        bounds=bounds,
+        compressor_params=compressor_params, 
+        B_integral=B_integral, 
+        render_mode=render_mode,
+        device=device,
+        # kwargs differentiate between different versions
+        env_kwargs=kwargs)
+        # wrapping using monitor
+    
     return env
